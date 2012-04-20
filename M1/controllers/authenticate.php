ab5f681995ff7a38d98c0d0c9ebcb1e7e24b0886
Osin <housseini.toumani@ysance.com>
2012-04-20 12:53:44 +0200

    Osin, maj

--- M1/controllers/authenticate.php
+++ M1/controllers/authenticate.php
@@ -3,9 +3,12 @@
   $users = User::find('all', array('conditions' => array('username' => strtolower($_POST['login']), 'password' => sha1($_POST['password']))));
   if (count($users)>0){
     $_SESSION['user'] = $users[0];
+    /*
     if (count(!$users[0]->getPrivileges()) == 0) {
       header("location:/M1/index.php?error=true;");
     }
+     * 
+     */
     header("location:/M1/public/hub.php");
   }
   else
