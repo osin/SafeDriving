c293b62dd1a9f9b55774d4ebbb4b101bd6068ec5
Osin <housseini.toumani@ysance.com>
2012-04-20 11:28:33 +0200

    Osin,

--- M1/blog/wp-config.php
+++ M1/blog/wp-config.php
@@ -19,16 +19,16 @@
 
 // ** Réglages MySQL - Votre hébergeur doit vous fournir ces informations. ** //
 /** Nom de la base de données de WordPress. */
-define('DB_NAME', 'M1db');
+define('DB_NAME', 'db_a0f0f372');
 
 /** Utilisateur de la base de données MySQL. */
-define('DB_USER', 'root');
+define('DB_USER', 'user_a0f0f372');
 
 /** Mot de passe de la base de données MySQL. */
-define('DB_PASSWORD', 'test');
+define('DB_PASSWORD', 'E5e&Bq_GNYDVLA');
 
 /** Adresse de l'hébergement MySQL. */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'a.db.shared.orchestra.io');
 
 /** Jeu de caractères à utiliser par la base de données lors de la création des tables. */
 define('DB_CHARSET', 'utf8');
